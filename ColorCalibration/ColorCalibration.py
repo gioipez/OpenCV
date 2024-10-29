87f5bec290c0404b038d4b94b153288b7df8f653
Giovanni López <gioipez92@gmail.com>
2024-10-28 19:13:42 -0500

    Update default color_checker

--- ColorCalibration/ColorCalibration.py
+++ ColorCalibration/ColorCalibration.py
@@ -78,11 +78,11 @@ class ColorCorrection:
         src /= 255
         return src
 
-    def create_color_correction_model(self, src, color_space=cv2.ccm.COLOR_SPACE_sRGB, ccm_type=cv2.ccm.CCM_3x3, gamma=2.2, degree=3, distance=cv2.ccm.DISTANCE_CIE2000, linearization=cv2.ccm.LINEARIZATION_GAMMA, saturated_threshold_min=0, saturated_threshold_max=0.98) -> cv2.ccm_ColorCorrectionModel:
+    def create_color_correction_model(self, src, color_checker=cv2.ccm.COLORCHECKER_Macbeth, color_space=cv2.ccm.COLOR_SPACE_sRGB, ccm_type=cv2.ccm.CCM_3x3, gamma=2.2, degree=3, distance=cv2.ccm.DISTANCE_CIE2000, linearization=cv2.ccm.LINEARIZATION_GAMMA, saturated_threshold_min=0, saturated_threshold_max=0.98) -> cv2.ccm_ColorCorrectionModel:
         """
         Create and configure the Color Correction Model (CCM).
         """
-        model = cv2.ccm_ColorCorrectionModel(src, cv2.ccm.COLORCHECKER_Macbeth)
+        model = cv2.ccm_ColorCorrectionModel(src, color_checker)
         model.setColorSpace(color_space)
         model.setCCM_TYPE(ccm_type)
         model.setDistance(distance)
